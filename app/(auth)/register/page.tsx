9808c57a2bf5d1211507eac053591867aa4de7d9
Ale <ale.ercolani@gmail.com>
2025-04-19 17:34:13 +0200

    diverge into hoodie

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -47,7 +47,7 @@ export default function RegisterPage() {
             >
               Privacy Policy
             </Link>
-            .
+            
           </p>
         </div>
       </div>
